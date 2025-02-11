99572b6ebb9d370a3d68bf8ef60f005df6c3d96a
William MacBook <wwilliamw24@gmail.com>
2025-02-11 13:28:38 -0800

    valentines

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
-import lovesvg from "./assets/All You Need Is Love SVG Cut File.svg";
-import lovesvg2 from "./assets/Love In The Air SVG Cut File.svg";
+import dogbeach from "/IMG_0620.JPG"
+import zouk from "/IMG_1167.JPG"
+import akpsi from "/IMG_4602.JPG"
+import centurycity from "/IMG_1045.JPG"
+// import angry from "./assets/IMG_7538.png"
 
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
@@ -14,24 +17,25 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
+      "Why would you say no! Are you sure?",
       "Really sure?",
-      "Think again!",
-      "Last chance!",
+      "Give it one more thought",
+      "Please?!",
       "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
+      "Last chance to be my valentine!",
+      "Kidding, but please?",
+      "Don't be silly! Just say yes!",
+      "You're supposed to say yes...",
+      "Come on!! Be nice >:(",
+      "Why you keep saying no?!",
+      "Change of heart perhaps?",
       "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Plsss? :( You're breaking my heart",
+      "I'm giving you one more chance",
+      "You're on thin ice...",
+      "Is that your final answer?...",
+      "You make me sad!!! :(",
+      "COME ON JUST SAY YES!!!",
+      "CLICK YES! LOOK HOW BIG IT IS"
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -39,29 +43,51 @@ export default function Page() {
 
   return (
     <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
-      {yesPressed ? (
-        <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="text-4xl md:text-6xl font-bold my-4">
-            Ok Yayyyyy!!!
-          </div>
-        </>
+      {yesPressed ? ( 
+        noCount == 0 ? (
+          <>
+            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+            <div className="text-4xl md:text-6xl font-bold my-4">
+              OKAY!!!! YAYYYYY!!!! HEHEHE
+            </div>
+            <div className="flex flex-row">
+              {/* <img src={angry} />
+              <img src={angry} />
+              <img src={angry} /> */}
+            </div>
+          </>
+        ) : (
+          <>
+            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+            <div className="text-4xl md:text-6xl font-bold my-4">
+              OKAY!!!! YAYYYYY!!!! FINALLY!!!! HEHEHE
+            </div>
+          </>         
+        )
       ) : (
         <>
           <img
-            src={lovesvg}
+            src={dogbeach}
             className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
           />
           <img
-            src={lovesvg2}
+            src={zouk}
             className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
+          />
+          <img
+            src={akpsi}
+            className="fixed bottom-16 left-10 md:left-24 -z-10 animate-pulse md:w-40 w-32"
+          />
+          <img
+            src={centurycity}
+            className="fixed top-16 right-10 md:right-24 -z-10 animate-pulse md:w-40 w-32"
           />
           <img
             className="h-[230px] rounded-lg shadow-lg"
             src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my Valentine?
+            Will you be my Valentine? <span role="img" aria-label="heart"> ❤️ </span>
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
@@ -86,16 +112,5 @@ export default function Page() {
 }
 
 const Footer = () => {
-  return (
-    <a
-      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
-      target="__blank"
-    >
-      Made with{" "}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>
-    </a>
-  );
+
 };
